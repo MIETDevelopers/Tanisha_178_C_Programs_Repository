e267da783548f417fd123a327f7e93a678c590f0
tanisha gandhi <14tanishagandhi14@gmail.com>
2021-03-18 16:35:19 +0530

    Hello While Loop

--- 3_helloloop.c
+++ 3_helloloop.c
@@ -1,10 +1,11 @@
+//Hello world program created using Do-While loop to print Hello world multiple times.
 #include <stdio.h>
 int main()
 {
     int a = 1;
-    while ( a <= 20 )
+    while ( a <= 20 )//This line will allow us to print hello world 10 times.
     {
-        printf ( "Hello World\n" );
+        printf ( "Hello World\n" );//Here we entered the texted to be printed.
         a ++;
     }
     return 0;

6701079611f384d5768639a8dac019bad8256c4d
tanisha gandhi <14tanishagandhi14@gmail.com>
2021-03-18 16:30:16 +0530

    Hello World

--- 1_Hello.c
+++ 1_Hello.c
@@ -1,5 +1,10 @@
+//A basic C Program to print Hello World.				
+//Tanisha Gandhi 18/March/2021
+//Start
 #include<stdio.h>
+ //Including standard input ouput header files from library.#include<stdio.h>
 int main(){
 	printf("Hello World");
 	return 0;
 	}
+	//End 

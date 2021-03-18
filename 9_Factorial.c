3dcfbc5894d601850056089c58da094c495c8f0b
tanisha gandhi <14tanishagandhi14@gmail.com>
2021-03-18 17:08:13 +0530

    Factorial

--- 9_Factorial.c
+++ 9_Factorial.c
@@ -1,9 +1,12 @@
+//Creation:18-03-21
+//Tanisha Gandhi 
+//A C Program to calculate Factorial of a number.
 #include<stdio.h>  
 int main()    
 {    
  int i,fact=1,number;    
  printf("Enter a number: ");    
-  scanf("%d",&number);    
+  scanf("%d",&number); //It will sacn the number entered by the user.   
     for(i=1;i<=number;i++){    
       fact=fact*i;    
   }    

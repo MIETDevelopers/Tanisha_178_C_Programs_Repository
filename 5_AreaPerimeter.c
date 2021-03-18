24ed7a371b622fa12e091b819649a54e7470a312
tanisha gandhi <14tanishagandhi14@gmail.com>
2021-03-18 16:47:16 +0530

    Area And Perimeter

--- 5_AreaPerimeter.c
+++ 5_AreaPerimeter.c
@@ -1,3 +1,9 @@
+//Creation:18-03-21 Tanisha Gandhi 
+//Perimeter of Circle: 2*(22/7)*r: radius
+//Area of Square: side*side: side
+//Area of Rectangle: Length * Breadth: Length, Breadth
+//Area of Circle: (22/7)*r*r: radius
+//Area of Circle: (22/7)*r*r: radius 
 #include<stdio.h>
 int main(){
 	float raduisOfCircle, sideOfSquare, legthOfRectangle, breadthOfRectangle;

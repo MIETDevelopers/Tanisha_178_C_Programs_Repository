557b5b7dc8749827be6ef6d51110ee36e043a55d
tanisha gandhi <14tanishagandhi14@gmail.com>
2021-03-18 16:42:14 +0530

    Arithmetic

--- 4_Arithmetic.c
+++ 4_Arithmetic.c
@@ -1,3 +1,4 @@
+//Creation date: 16-03-21
 #include<stdio.h>
 int main(){
     char operator;
@@ -9,19 +10,19 @@ int main(){
     switch(operator)
     {
         case '+':
-            printf("%.lf + %.lf = %.lf",a, b, a+b);
+            printf("%.lf + %.lf = %.lf",a, b, a+b);//Addition Function
             break;
 
         case '-':
-            printf("%.lf - %.lf = %.lf",a, b, a-b);
+            printf("%.lf - %.lf = %.lf",a, b, a-b);//Subtraction Function
             break;
 
         case '*':
-            printf("%.lf * %.lf = %.lf",a, b, a*b);
+            printf("%.lf * %.lf = %.lf",a, b, a*b);//Multiplication Function
             break;
 
         case '/':
-            printf("%.lf / %.lf = %.lf",a, b, a/b);
+            printf("%.lf / %.lf = %.lf",a, b, a/b);//Division Function
             break;
     printf("Error encountered by the input values");
     }
